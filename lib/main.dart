695dd44d1917b86c4ed56b4c22d1a68eccda1e33
rromaniz <romaniz.avila@gmail.com>
2021-06-01 10:59:46 -0500

    Se cambio el title del archivo main

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,16 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white54,
-        appBar:AppBar(
-          title: Text('Soy Rico'),
-          centerTitle: true,
-          backgroundColor: Color.fromRGBO(68, 44, 105, 0.5)
-        ),
+        appBar: AppBar(
+            title: Text('Soy muy rico'),
+            centerTitle: true,
+            backgroundColor: Color.fromRGBO(68, 44, 105, 0.5)),
         body: Center(
-          child: Image(
-             image: AssetImage('images/diamond.png'),
-            alignment: Alignment.center,)
-        ),
-        
-        ),
+            child: Image(
+          image: AssetImage('images/diamond.png'),
+          alignment: Alignment.center,
+        )),
+      ),
     ),
   );
 }
